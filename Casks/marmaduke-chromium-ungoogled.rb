a661bdcfb144585c22e703d40bccc973b2fd61e3
Tyler W <19383474+leftl@users.noreply.github.com>
2020-09-07 15:04:07 -0700

    sha256sum on archive fixed
    accidentally got sum on contained .app file.

--- Casks/marmaduke-chromium-ungoogled.rb
+++ Casks/marmaduke-chromium-ungoogled.rb
@@ -1,6 +1,6 @@
 cask 'marmaduke-chromium-ungoogled' do
   version '85.0.4183.83'
-  sha256 'e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855'
+  sha256 'f64537a49a82f486800522781b4bcab892f941067cee5479f04c88f3b4e3fdbe'
 
   url "https://github.com/macchrome/macstable/releases/download/v#{version}-r782793-Ungoogled-macOS/Chromium.app.ungoogled-#{version}.zip"
   appcast 'https://github.com/macchrome/macstable/releases.atom'
