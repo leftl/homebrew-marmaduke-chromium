c437ed71a821fd8215babf8cddd2662b95bbdee7
Tyler W <19383474+leftl@users.noreply.github.com>
2020-09-07 14:42:11 -0700

    Update marmaduke-chromium-ungoogled.rb
    update ungoogled chromium

--- Casks/marmaduke-chromium-ungoogled.rb
+++ Casks/marmaduke-chromium-ungoogled.rb
@@ -1,8 +1,8 @@
 cask 'marmaduke-chromium-ungoogled' do
-  version '84.0.4147.125'
-  sha256 '41ac24152e99d34c2e1e4f36e2a7e1152f9fd6b1cd971ef8bbd0ac1c225b2add'
+  version '85.0.4183.83'
+  sha256 'e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855'
 
-  url "https://github.com/macchrome/macstable/releases/download/v#{version}-r768962-Ungoogled-macOS/Chromium.app.ungoogled-#{version}.zip"
+  url "https://github.com/macchrome/macstable/releases/download/v#{version}-r782793-Ungoogled-macOS/Chromium.app.ungoogled-#{version}.zip"
   appcast 'https://github.com/macchrome/macstable/releases.atom'
   name 'Chromium'
   homepage 'https://github.com/macchrome/macstable/releases'
